ef80e7484c66644799c18d0110a2c447305e7eb5
popeye <popeye0618@gmail.com>
2026-05-26 13:43:31 +0900

    feat: Maven Central 배포 설정 추가
    - `com.vanniktech.maven.publish` 플러그인 추가 및 Maven Central 배포 구성
    - 프로젝트 전반에 POM 메타데이터 설정 추가 (라이센스, 개발자 정보, SCM 등)
    - springforge-core 및 springforge-web 모듈에 Maven Publishing 설정 추가
    - 버전 정보 `0.1.0-SNAPSHOT`에서 `0.1.0`으로 업데이트하여 릴리스 준비 완료

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@ plugins {
 	kotlin("jvm") version "1.9.25" apply false
 	kotlin("plugin.spring") version "1.9.25" apply false
 	id("io.spring.dependency-management") version "1.1.7" apply false
+	id("com.vanniktech.maven.publish") version "0.30.0" apply false
 }
 
 group = "io.github.popeye0618.springforge"
-version = "0.1.0-SNAPSHOT"
+version = "0.1.0"
 description = "Springforge"
 
 allprojects {
@@ -28,5 +29,40 @@ subprojects {
 
 	tasks.withType<Test> {
 		useJUnitPlatform()
+	}
+
+	plugins.withId("com.vanniktech.maven.publish") {
+		configure<com.vanniktech.maven.publish.MavenPublishBaseExtension> {
+			publishToMavenCentral(automaticRelease = true)
+			signAllPublications()
+
+			pom {
+				url.set("https://github.com/popeye0618/springforge")
+				inceptionYear.set("2026")
+
+				licenses {
+					license {
+						name.set("MIT License")
+						url.set("https://opensource.org/licenses/MIT")
+						distribution.set("https://opensource.org/licenses/MIT")
+					}
+				}
+
+				developers {
+					developer {
+						id.set("popeye0618")
+						name.set("Seunghwan Chun")
+						email.set("popeye0618@gmail.com")
+						url.set("https://github.com/popeye0618")
+					}
+				}
+
+				scm {
+					url.set("https://github.com/popeye0618/springforge")
+					connection.set("scm:git:https://github.com/popeye0618/springforge.git")
+					developerConnection.set("scm:git:ssh://git@github.com/popeye0618/springforge.git")
+				}
+			}
+		}
 	}
 }
